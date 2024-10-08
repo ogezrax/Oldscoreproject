c82699bac37ef66fd927ba16f55ea06faf4792c8
IFCE-Lab4 <esdras.brito09@aluno.ifce.edu.br>
2024-10-08 11:18:40 -0300

    Esdras de Souza Brito; Alex Oliveira Carvalho; Arthur Ferreira do Nascimento Barros

---
+++ app/src/main/java/com/example/oldscoreproject/teladelogin.java
@@ -0,0 +1,14 @@
+package com.example.oldscoreproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class teladelogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teladelogin);
+    }
+}
